e4fc412988e1487cc01261514b5dfc3f92fec71b
alherdom <alherdom@outlook.com>
2023-09-21 13:58:27 +0100

    14 passed

--- ut0/max_product.py
+++ ut0/max_product.py
@@ -1,13 +1,18 @@
-import sys
-
-def mult(numbers: list[int]) -> int:
+def mult(numbers: str) -> int:
     result = 1
     for number in numbers:
-        result *= number
+        result *= int(number)
     return result
 
 def max_product(sequence: str, span: int) -> int:
     if span > len(sequence) or span <= 0 or not sequence.isnumeric():
         raise ValueError("Invalid input arguments!")
+    return max([mult(sequence[i:i+span]) for i, char in enumerate(sequence) if i <= len(sequence) -span])
     
-    
+    # Sin comprensión y usando 'break':
+    # parts_sequence = []
+    # for i, char in enumerate(sequence):
+    #     parts_sequence.append(mult(sequence[i:i+span]))
+    #     if i == len(sequence) - span:
+    #         break
+    # return max(parts_sequence)
